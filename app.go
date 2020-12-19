29e80a594b85c45be9470f53cdd404dd837b360d
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-12-20 00:44:02 +0530

    Removed MUX and implemented basic contact tracing ^-^

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-	"github.com/gorilla/mux"
+	"strings"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -19,13 +19,12 @@ var dbName = "users"
 
 // App ...
 type App struct {
-	Router *mux.Router
 	DB     *mongo.Database
 }
 
 // Run ...
 func (app *App) Run(address string) {
-	log.Fatal(http.ListenAndServe(address, app.Router))
+	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 // Initialize ...
@@ -36,7 +35,6 @@ func (app *App) Initialize(_user, _password string) {
 	app.DB, _ = app.configDB(ctx)
 	fmt.Println("Connected to MongoDB!")
 
-	app.Router = mux.NewRouter()
 	app.initializeRoutes()
 }
 
@@ -55,86 +53,134 @@ func (app *App) configDB(ctx context.Context) (*mongo.Database, error) {
 
 // routing
 func (app *App) initializeRoutes() {
-	app.Router.HandleFunc("/users", app.getUsers).Methods("GET")
-	app.Router.HandleFunc("/users/{id}", app.getUser).Methods("GET")
-	app.Router.HandleFunc("/users", app.createUser).Methods("POST")
-	app.Router.HandleFunc("/contacts", app.createContact).Methods("POST")
+	http.HandleFunc("/allUsers", app.getUsers)
+	http.HandleFunc("/users/", app.getUser)
+	http.HandleFunc("/users", app.createUser)
+	http.HandleFunc("/contacts", app.contact)
+	
 }
 
 func (app *App) getUsers(writer http.ResponseWriter, req *http.Request) {
-	count, _ := strconv.Atoi(req.FormValue("count"))
-	start, _ := strconv.Atoi(req.FormValue("start"))
-
-	if count > 10 || count < 1 {
-		count = 10
-	}
-	if start < 0 {
-		start = 0
-	}
-
-	bs, err := getUsers(app.DB, start, count)
-	if err != nil {
-		respondWithError(writer, http.StatusInternalServerError, err.Error())
+	
+	if req.Method == "GET" {
+		count, _ := strconv.Atoi(req.FormValue("count"))
+		start, _ := strconv.Atoi(req.FormValue("start"))
+	
+		if count > 10 || count < 1 {
+			count = 10
+		}
+		if start < 0 {
+			start = 0
+		}
+	
+		users, err := getUsers(app.DB, start, count)
+		if err != nil {
+			respondWithError(writer, http.StatusInternalServerError, err.Error())
+			return
+		}
+	
+		respondWithJSON(writer, http.StatusOK, users)
+	} else {
+		respondWithError(writer, http.StatusInternalServerError, req.Method + " Method not allowed, try with GET")
 		return
-	}
-
-	respondWithJSON(writer, http.StatusOK, bs)
+	}	
 }
 
 func (app *App) getUser(writer http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	id, _ := primitive.ObjectIDFromHex(vars["id"])
+	if req.Method == "GET" {
+		varID := strings.SplitN(req.URL.Path, "/", 3)[2]
+		id, _ := primitive.ObjectIDFromHex(varID)
 
-	user := User{ID: id}
-	if err := user.getUser(app.DB); err != nil {
-		switch err {
-		case mongo.ErrNoDocuments:
-			respondWithError(writer, http.StatusNotFound, "User not found")
-		default:
-			respondWithError(writer, http.StatusInternalServerError, err.Error())
+		user := User{ID: id}
+		if err := user.getUser(app.DB); err != nil {
+			switch err {
+			case mongo.ErrNoDocuments:
+				respondWithError(writer, http.StatusNotFound, "User not found")
+			default:
+				respondWithError(writer, http.StatusInternalServerError, err.Error())
+			}
+			return
 		}
+
+		respondWithJSON(writer, http.StatusOK, user)
+	} else {
+		respondWithError(writer, http.StatusInternalServerError, req.Method + " Method not allowed, try with GET")
 		return
 	}
-
-	respondWithJSON(writer, http.StatusOK, user)
 }
 
 func (app *App) createUser(writer http.ResponseWriter, req *http.Request) {
-	var user User
+	
+	if req.Method == "POST" {
+		var user User
+	
+		decoder := json.NewDecoder(req.Body)
+		if err := decoder.Decode(&user); err != nil {
+			respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+			return
+		}
+		defer req.Body.Close()
 
-	decoder := json.NewDecoder(req.Body)
-	if err := decoder.Decode(&user); err != nil {
-		respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+		result, err := user.createUser(app.DB)
+		if err != nil {
+			respondWithError(writer, http.StatusInternalServerError, err.Error())
+			return
+		}
+		user.ID = result.InsertedID.(primitive.ObjectID)
+		respondWithJSON(writer, http.StatusCreated, user)
+
+	} else {
+		respondWithError(writer, http.StatusInternalServerError, req.Method + " Method not allowed, try with POST")
 		return
 	}
-	defer req.Body.Close()
-
-	result, err := user.createUser(app.DB)
-	if err != nil {
-		respondWithError(writer, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(writer, http.StatusCreated, result)
 }
 
-func (app *App) createContact(writer http.ResponseWriter, req *http.Request) {
+func (app *App) contact(writer http.ResponseWriter, req *http.Request) {
 	var contact Contact
+	
+	if req.Method == "POST" {
+		decoder := json.NewDecoder(req.Body)
+		if err := decoder.Decode(&contact); err != nil {
+			respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+			return
+		}
+		defer req.Body.Close()
+	
+		result, err := contact.createContact(app.DB)
+		if err != nil {
+			respondWithError(writer, http.StatusInternalServerError, err.Error())
+			return
+		}
+		contact.ID = result.InsertedID.(primitive.ObjectID)
+		respondWithJSON(writer, http.StatusCreated, contact)
 
-	decoder := json.NewDecoder(req.Body)
-	if err := decoder.Decode(&contact); err != nil {
-		respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+	} else if req.Method == "GET" {	
+
+		user := req.URL.Query()["user"][0]
+		infectionTimestamp := req.URL.Query()["infection_timestamp"][0]
+
+		count, _ := strconv.Atoi(req.FormValue("count"))
+		start, _ := strconv.Atoi(req.FormValue("start"))
+		req.Method = "GET"
+
+		if count > 10 || count < 1 {
+			count = 10
+		}
+		if start < 0 {
+			start = 0
+		}
+
+		users, err := getContactTracing(app.DB, start, count, user, infectionTimestamp)
+		if err != nil {
+			respondWithError(writer, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		respondWithJSON(writer, http.StatusOK, users)
+	} else {
+		respondWithError(writer, http.StatusInternalServerError, req.Method + " Method not allowed, try with POST")
 		return
-	}
-	defer req.Body.Close()
-
-	result, err := contact.createContact(app.DB)
-	if err != nil {
-		respondWithError(writer, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(writer, http.StatusCreated, result)
+	}	
 }
 
 // helpers
--- contact.go
+++ contact.go
@@ -5,6 +5,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
+	"fmt"
 )
 
 // Contact ...
@@ -17,36 +18,69 @@ type Contact struct {
 
 var contactCollectionName = "contacts"
 
-func getContacts(db *mongo.Database, start, count int) ([]Contact, error) {
-	col := contactsCollection(db)
+func getContactTracing(db *mongo.Database, start, count int, userID string, timestamp string) ([]User, error) {
+	contactCol := contactsCollection(db)
+	userCol := usersCollection(db)
 	ctx := dbContext(30)
 
-	cursor, err := col.Find(ctx, bson.M{}) // find all
+	cursor, err := contactCol.Find(ctx, bson.M{}) // find all
 	if err != nil {
 		return nil, err
 	}
 	defer cursor.Close(ctx)
 
-	var contacts []Contact
+	var users []User
 	for cursor.Next(ctx) {
 		var contact Contact
 		cursor.Decode(&contact)
-		contacts = append(contacts, contact)
+		if userID == contact.ContactIDOne || userID == contact.ContactIDTwo{
+			layout := "2006-01-02T15:04:05.000Z"
+			given, err := time.Parse(layout, timestamp)
+			
+			if err != nil{
+				continue
+			}
+			
+			fourteenDaysAgo := given.AddDate(0, 0, -14)
+			
+			if  contact.TimeStamp.After(fourteenDaysAgo) && contact.TimeStamp.Before(given) {
+				var user User
+				if userID == contact.ContactIDOne {
+					objectID, err := primitive.ObjectIDFromHex(contact.ContactIDTwo)
+					if err != nil{
+						fmt.Println(err)
+					}
+					
+					result:= userCol.FindOne(ctx, bson.M{"_id": objectID})
+					result.Decode(&user)				
+					users = append(users, user)
+				} else {
+					objectID, err := primitive.ObjectIDFromHex(userID)
+					if err != nil{
+						fmt.Println(err)
+					}
+					
+					result:= userCol.FindOne(ctx, bson.M{"_id": objectID})
+					result.Decode(&user)				
+					users = append(users, user)
+				}
+			}
+		}
 	}
 
 	if err := cursor.Err(); err != nil {
 		return nil, err
 	}
 
-	return contacts, nil
+	return users, nil
 }
 
 func (contact *Contact) createContact(db *mongo.Database) (*mongo.InsertOneResult, error) {
-	col := contactsCollection(db)
+	contactCol := contactsCollection(db)
 	ctx := dbContext(30)
 	contact.TimeStamp = time.Now()
 
-	result, err := col.InsertOne(ctx, contact)
+	result, err := contactCol.InsertOne(ctx, contact)
 	return result, err
 }
 
